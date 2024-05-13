0ca9c1509efe2ef52b422005493f86c54a98caa8
iwas108 <64204327+iwas108@users.noreply.github.com>
2024-05-13 19:38:24 +0800

    Checkpoint

--- lib/UrusanAktuatorLingkungan/src/UrusanAktuatorLingkungan.h
+++ lib/UrusanAktuatorLingkungan/src/UrusanAktuatorLingkungan.h
@@ -1,17 +1,21 @@
 #ifndef UrusanAktuatorLingkungan_h
 #define UrusanAktuatorLingkungan_h
 
-#include <Wire.h>
-#include <Adafruit_SHT31.h>
+#include "Arduino.h"
 
 class UrusanAktuatorLingkungan {
     public:
-        UrusanAktuatorLingkungan();
-        bool mulai();
-        float nyalakan(uint8_t kekuatan, bool arah);
+        UrusanAktuatorLingkungan(uint8_t ina, uint8_t inb);
+        void mulai();
+        void nyalakan(uint8_t kekuatan, bool arah);
         void padamkan();
+        float bacaKekuatan();
+        float bacaArah();
+        void anginTopan(bool arah);
     
     private:
+        uint8_t _ina;
+        uint8_t _inb;
         float _kekuatan;
         float _arah;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,17 @@
 #include <UrusanIoT.h>
 #include "secret.h"
 #include <TaskScheduler.h>
-#include <UrusanSensorLingkungan.h>
+#include <UrusanAktuatorLingkungan.h>
 #include <ArduinoJson.h>
 
 
 void penangkapPesan(String topic, String message);
 void task1DetailTugas();
+void subscribeTopik();
 
 UrusanWiFi urusanWiFi(ssid, pass);
 UrusanIoT urusanIoT(broker, port, id, brokerUsername, brokerPassword);
-UrusanSensorLingkungan urusanSensorLingkungan;
+UrusanAktuatorLingkungan urusanAktuatorLingkungan(32, 33);
 Scheduler penjadwal;
 
 Task task1(3000, TASK_FOREVER, &task1DetailTugas);
@@ -25,20 +26,14 @@ void setup() {
   Serial.begin(115200);
 
   urusanWiFi.konek();
-  while(urusanWiFi.apakahKonek() == 0){
-    Serial.print(".");
-    delay(1000);
-  }
   urusanIoT.konek();
-  while(urusanIoT.apakahKonek() == 0){
-    Serial.print(".");
-    delay(1000);
-  }
-  
   urusanIoT.penangkapPesan(penangkapPesan);
-  urusanIoT.subscribe("tld/namaorganisasi/namadivisi/setelan");
 
-  urusanSensorLingkungan.mulai();
+  if(urusanIoT.apakahKonek() == 1){
+    subscribeTopik();
+  }
+
+  urusanAktuatorLingkungan.mulai();
 
   penjadwal.init();
   penjadwal.addTask(task1);
@@ -51,30 +46,62 @@ void loop() {
 
   if(urusanWiFi.apakahKonek() == true && urusanIoT.apakahKonek() == false){
     urusanIoT.konek();
+    if(urusanIoT.apakahKonek() == 1){
+      subscribeTopik();
+    }
   }
 
   penjadwal.execute();
 }
 
+void subscribeTopik(){
+  urusanIoT.subscribe("tld/namaorganisasi/namadivisi/setelan");
+}
+
 void penangkapPesan(String topic, String message){
   Serial.printf("penangkapPesan: topic: %s | message: %s\n", topic.c_str(), message.c_str());
+
+  JsonDocument dataMasuk;
+  DeserializationError galatParseJson = deserializeJson(dataMasuk, message);
+  if(galatParseJson == DeserializationError::Ok){
+    if(dataMasuk["perintah"] != nullptr){
+      String perintah = dataMasuk["perintah"].as<String>();
+
+      if(perintah == String("nyalakan")){
+        if( dataMasuk["arah"] != nullptr && dataMasuk["kekuatan"] != nullptr){
+          bool arah = dataMasuk["arah"].as<bool>();
+          uint8_t kekuatan = dataMasuk["kekuatan"].as<uint8_t>();
+          urusanAktuatorLingkungan.nyalakan(kekuatan, arah);
+        }
+      }
+      else if(perintah == String("anginTopan")){
+        if( dataMasuk["arah"] != nullptr){
+          bool arah = dataMasuk["arah"].as<bool>();
+          urusanAktuatorLingkungan.anginTopan(arah);
+        }
+      }
+      else if(perintah == String("padamkan")){
+        urusanAktuatorLingkungan.padamkan();
+      }
+    }
+    
+  }
+  else{
+    Serial.println("penangkapPesan: Format pesan tidak valid! Gunakan format JSON.");
+  }
 }
 
 void task1DetailTugas(){
   if(urusanIoT.apakahKonek() == true){
-    if(urusanSensorLingkungan.apakahSensorSiap() == true){
-      float suhu = urusanSensorLingkungan.bacaSuhu();
-      float kelembapan = urusanSensorLingkungan.bacaKelembapan();
+    JsonDocument data;
+    char muatan[512];
 
-      JsonDocument data;
-      char muatan[512];
+    data["arah"] = urusanAktuatorLingkungan.bacaArah();
+    data["kekuatan"] = urusanAktuatorLingkungan.bacaKekuatan();
 
-      data["suhu"] = suhu;
-      data["kelembapan"] = kelembapan;
+    serializeJson(data, muatan);
 
-      serializeJson(data, muatan);
-
-      urusanIoT.publish("tld/namaorganisasi/namadivisi", muatan);
-    }
+    urusanIoT.publish("tld/namaorganisasi/namadivisi", muatan);
+  
   }
 }
