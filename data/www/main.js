f9e7ba0f5c7b28a17e8423848258a7683075a60d
iwas108 <64204327+iwas108@users.noreply.github.com>
2024-05-21 13:35:23 +0800

    adde generateRandomString

--- data/www/main.js
+++ data/www/main.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function generateRandomString(length) {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+    }
+
     // Buat instan client: alamat Broker, Port, Websocket Path, Client ID
+    var klienIdUnik = "gantinamaperusahaan.tld-gantinamadivisi-browser" + generateRandomString(5);
     client = new Paho.MQTT.Client("broker.hivemq.com", Number(8000), "/mqtt", "gantinamaperusahaan.tld-gantinamadivisi-browser");
 
     // setel penangkap panggilan balik
